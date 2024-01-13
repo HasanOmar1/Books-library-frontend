54da18619b7ae061bc85b9a6aad2f4aa6374a570
HasanOmar1 <r3domar1@gmail.com>
2024-01-13 14:32:17 +0200

    added

---
+++ src/components/BooksCards/LibraryBooks/LibraryBooks.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import "./LibraryBooks.css";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+export default function LibraryBooks({ library }) {
+  return (
+    <div className="LibraryBooks">
+      <div className="library-container">
+        {library?.map((info) => {
+          return (
+            <div className="info-container">
+              <div className="library-book-container">
+                <div className="img-and-button">
+                  <img
+                    src={info?.volumeInfo?.imageLinks?.thumbnail}
+                    alt={info?.volumeInfo?.title}
+                  />
+                  <Link
+                    key={info?._id}
+                    to={`${info?.volumeInfo?.title}`}
+                    state={info}
+                  >
+                    <Button variant="primary">More Info</Button>
+                  </Link>
+                </div>
+                <div className="books-more-info">
+                  <p>{info?.volumeInfo?.title}</p>
+                  <p>
+                    <span className="span-title">Written by </span>
+                    {info?.volumeInfo?.authors}
+                  </p>
+                  <p>
+                    <span className="span-title">Published by </span>
+                    {info?.volumeInfo?.publisher}
+                  </p>
+                  <div className="category">
+                    <span className="span-title">Category</span>
+                    <p>{info?.volumeInfo?.categories}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- src/pages/MyLibrary/MyLibrary.jsx
+++ src/pages/MyLibrary/MyLibrary.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./MyLibrary.css";
 import { useLibraryContext } from "../../Context/LibraryContext";
+import { useNewUsersContext } from "../../Context/NewUsersContext";
+import LibraryBooks from "../../components/BooksCards/LibraryBooks/LibraryBooks";
 
 export default function MyLibrary() {
   const { libraryBooks, setLibraryBooks } = useLibraryContext();
+  const { currentUser } = useNewUsersContext();
 
-  console.log(libraryBooks);
+  useEffect(() => {
+    console.log(currentUser?.books);
+  }, [currentUser?.books]);
+
   return (
     <main className="MyLibrary">
       <h4 className="title">My Library</h4>
-      {/* <div className="library-container">{libraryBooks}</div> */}
+      <div className="library-container">
+        <LibraryBooks library={currentUser?.books} />
+      </div>
     </main>
   );
 }
