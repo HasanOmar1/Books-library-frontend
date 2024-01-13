8ebaf46020e61a386f53845e829ef6045fe5e42f
HasanOmar1 <r3domar1@gmail.com>
2024-01-13 16:30:16 +0200

    added remove from library

--- src/Context/NewUsersContext.jsx
+++ src/Context/NewUsersContext.jsx
@@ -33,7 +33,6 @@ export default function NewUsersProvider({ children }) {
       console.log(response.data);
       setCurrentUser(response.data);
       const userJSON = JSON.stringify(response.data);
-      console.log(response.data.token);
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("user", userJSON);
 
