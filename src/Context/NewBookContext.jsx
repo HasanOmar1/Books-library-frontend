bf9e1ad6d5f2ec9eb9b8fdd67d47971c6bc316c3
HasanOmar1 <r3domar1@gmail.com>
2024-01-24 16:45:40 +0200

    nothing

--- src/Context/NewBookContext.jsx
+++ src/Context/NewBookContext.jsx
@@ -36,7 +36,7 @@ export default function NewBookProvider({ children }) {
       }
       getFairyBooks();
     } catch (error) {
-      console.log(error);
+      console.log(error.response.data);
     }
   }
 
